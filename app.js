1cca325080cfe7e4248a944551ceba530a30d799
mdshohidulislam1486 <74629151+mdshohidulislam1486@users.noreply.github.com>
2021-09-04 16:36:45 +0600

    updated

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log(window)
+const loadWord = ()=>{
+    const search = document.getElementById('search');
+    const searchText= search.value;
+    const URL = (`https://api.dictionaryapi.dev/api/v2/entries/en/${searchText}`)
+
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => console.log(data[0]))
+}
